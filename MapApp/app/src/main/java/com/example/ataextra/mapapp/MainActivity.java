17f8d0bb57dd4702c28c5ff37f940e85a8dc527f
avolchko <volchko.angie@gmail.com>
2016-11-19 19:53:34 -0500

    MapGraphics
    Basic interface to Map Graphics

---
+++ MapApp/app/src/main/java/com/example/ataextra/mapapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ataextra.mapapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, CloudDataPullActivity.class);
+      //  EditText editText = (EditText) findViewById(R.id.edit_message);
+       // String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
